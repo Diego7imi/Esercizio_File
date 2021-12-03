3727dffa9095c8115af8648c51b37e2d44e83db9
Diego Settimi <S1098119@studenti.univpm.it>
2021-12-03 08:55:06 +0100

    Added FileReader and method CSVtoVector

--- FileCSV/src/Parsing.java
+++ FileCSV/src/Parsing.java
@@ -1,9 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
 
 public class Parsing {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	final static String Delimeter = ",";
+	
+	public static void main(String[] args) 
+	{
+		String fileToParse = "employee.csv";
+		BufferedReader fileReader = null;
+		
+		try 
+		{
+			String line = "";
+			fileReader = new BufferedReader(new FileReader(fileToParse)); 
+			while((line = fileReader.readLine()) != null)
+			{
+				
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void companyCSVtoVector(String line)
+	{
+		
 	}
 
 }
