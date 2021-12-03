e18be3029336bdbc1399ab4f5372b5a6dd6145c2
Diego Settimi <S1098119@studenti.univpm.it>
2021-12-03 09:46:36 +0100

    Costruttore RegistroEtaCompleto

--- Registro/src/RegistroEtaCompleto.java
+++ Registro/src/RegistroEtaCompleto.java
@@ -1,4 +1,10 @@
-
+import java.util.Vector;
 public class RegistroEtaCompleto {
-
+	
+	Vector<NomeEta> studenti = new Vector<NomeEta>();
+	
+	public RegistroEtaCompleto(Vector<NomeEta> studente)
+	{
+		this.studenti = studente;
+	}
 }
