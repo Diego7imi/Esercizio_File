665e9078c9e170f7101790562cbc2135ae80b8e2
Diego Settimi <S1098119@studenti.univpm.it>
2021-12-03 09:43:05 +0100

    Finalizzazione classe NomeEta

--- Registro/src/NomeEta.java
+++ Registro/src/NomeEta.java
@@ -1,4 +1,14 @@
 
 public class NomeEta {
-
+	
+	String nome;
+	int eta;
+	
+	public NomeEta(String nome, int eta)
+	{
+		this.nome = nome;
+		this.eta = eta;
+	}
+	
+	public void setEta(int eta) {eta = this.eta + 1;}
 }
