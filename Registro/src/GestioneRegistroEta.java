812f48790255fc54b5bfdbfda3b40dc7d5807867
Diego Settimi <S1098119@studenti.univpm.it>
2021-12-04 11:25:14 +0100

    Inizio scrittura main

--- Registro/src/GestioneRegistroEta.java
+++ Registro/src/GestioneRegistroEta.java
@@ -1,9 +1,41 @@
-
+import java.util.Scanner;
+import java.io.*;
 public class GestioneRegistroEta {
 
 	public static void main(String[] args) 
 	{
 		
+		String nomefile, risp;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("BENVENUTO NEL PROGRAMMA GESTIONE REGISTRO");
+		System.out.print("\nInsersci nome file registro: ");
+		nomefile = input.next();
+		
+		RegistroEtaCompleto re = new RegistroEtaCompleto(nomefile);
+		
+		do 
+		{
+			System.out.println("\nA) Visualizza registro");
+			System.out.println("B) Aggiungi elemento");
+			System.out.println("C) Incrementa eta'");
+			System.out.println("D) Salva dati");
+			System.out.println("E) Esci");
+			System.out.print("Scelta: ");
+			risp = input.next();
+			
+			switch(risp)
+			{
+			case "a":
+				re.visualizza();
+				break;
+			}
+		}while(!risp.equals("e"));
+		
+		
+		
+		input.close();
 	}
 
 }
