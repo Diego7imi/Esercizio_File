406d7e9fd08906bd4fed993a434778d6d9ad441b
Diego Settimi <S1098119@studenti.univpm.it>
2021-12-04 10:46:30 +0100

    RegistroEtaCompleto costruttore completato

--- Registro/src/RegistroEtaCompleto.java
+++ Registro/src/RegistroEtaCompleto.java
@@ -10,17 +10,21 @@ public class RegistroEtaCompleto {
 		this.nomefile = nomefile;
 		try {
 			ObjectInputStream file = new ObjectInputStream(new BufferedInputStream(new FileInputStream(nomefile)));
-			studenti = (Vector<NomeEta>) file.readObject();
+			studenti = (Vector<NomeEta>)file.readObject();
 			file.close();
 		}catch(FileNotFoundException e)
 		{
-			
+			System.out.println("ATTENZIONE: Il file " + nomefile + " non esiste");
+			System.out.println("Sarà creato al primo salvataggio");
+			System.out.println();
 		}catch(ClassNotFoundException e)
 		{
-			
+			System.out.println("Errore di lettura");
+			System.out.println(e);
 		}catch(IOException e)
 		{
-			
+			System.out.println("ERRORE di I/O");
+			System.out.println(e);
 		}
 	}
 }
