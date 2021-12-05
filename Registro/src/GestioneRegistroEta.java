880f5b559cb13639c085f76cd9a02321677d6eee
Diego Settimi <S1098119@studenti.univpm.it>
2021-12-05 16:32:22 +0100

    Scrittura switch e aggiornamento files

--- Registro/src/GestioneRegistroEta.java
+++ Registro/src/GestioneRegistroEta.java
@@ -5,13 +5,15 @@ public class GestioneRegistroEta {
 	public static void main(String[] args) 
 	{
 		
-		String nomefile, risp;
+		String nomefile, risp, nome;
+		int eta;
 		
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("BENVENUTO NEL PROGRAMMA GESTIONE REGISTRO");
-		System.out.print("\nInsersci nome file registro: ");
-		nomefile = input.next();
+		System.out.print("\nInserisci nome file registro: ");
+		nomefile = input.nextLine();
+		//System.out.print(nomefile);
 		
 		RegistroEtaCompleto re = new RegistroEtaCompleto(nomefile);
 		
@@ -30,6 +32,29 @@ public class GestioneRegistroEta {
 			case "a":
 				re.visualizza();
 				break;
+			case "b":
+				System.out.print("Inserisci nome: ");
+				nome = input.next();
+				System.out.print("Inserisci eta': ");
+				eta = input.nextInt();
+				re.inserisci(nome, eta);
+				break;
+			case "c":
+				System.out.print("Inserisci nome: ");
+				nome = input.next();
+				re.incrementaEta(nome);
+				break;
+			case "d":
+				if(re.daSalvare())
+					if(re.salva())
+						System.out.println("\nModifica salvata con successo!");
+					else System.out.println("File già aggiornato");
+				else System.out.println("Non c'è stata alcuna modifica da salvare");
+				break;
+			case "e":
+				System.out.println("Arrivederci");
+				break;
+				default: System.out.println("Lettera non riconosciuta");
 			}
 		}while(!risp.equals("e"));
 		
